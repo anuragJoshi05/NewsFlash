827625ecd12c4a08a1796e82281f705405530f56
anuragJoshi05 <anurag88787@gmail.com>
2024-05-30 00:44:19 +0530

    Initial commit: Set up Flutter project with Home screen
    - Created a new Flutter project named "NewsFlash"
    - Implemented the main application structure in main.dart
    - Added a basic Home screen in home.dart
    - Customized the search bar with updated colors and icons

---
+++ lib/home.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController searchController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text("NewsFlash"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          SafeArea(
+              child:Container(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                margin: const EdgeInsets.symmetric(
+                    horizontal: 24, vertical: 20),
+                decoration: BoxDecoration(
+                  color: Colors.black, // Updated background color to black
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        margin: const EdgeInsets.fromLTRB(3, 0, 7, 0),
+                        child: const Icon(
+                          Icons.search,
+                          color:
+                          Colors.yellow, // Updated icon color to yellow
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextField(
+                        controller: searchController,
+                        style: const TextStyle(color: Colors.greenAccent),
+                        onSubmitted: (value) {},
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "BreakingNews",
+                          hintStyle: const TextStyle(
+                              color: Colors
+                                  .yellow), // Updated hint text color to yellow
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              // Add your action here
+                            },
+                            icon: const Icon(
+                              Icons.favorite,
+                              color: Colors
+                                  .yellow, // Updated icon color to yellow
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          )
+        ],
+      ),
+    );
+  }
+}
