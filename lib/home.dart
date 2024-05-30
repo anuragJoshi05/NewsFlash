7d990cfaaba6990b22c569d040b10d4279f1617b
Anurag Joshi <anurag88787@gmail.com>
2024-05-30 17:26:01 +0530

    feat: Add News Cards and Enhance Carousel and Navigation
    - Integrated scrollable news cards with images and headlines
    - Enhanced carousel slider with new content and improved layout
    - Improved navigation bar styling and interactivity
    - Updated search bar styling with enhanced hint text and icon color
    - General UI improvements and code optimizations

--- lib/home.dart
+++ lib/home.dart
@@ -31,114 +31,180 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.black87,
       ),
-      body: Column(
-        children: [
-          SafeArea(
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-              decoration: BoxDecoration(
-                color: Colors.black, // Updated background color to black
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: Row(
-                children: [
-                  GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      margin: const EdgeInsets.fromLTRB(3, 0, 7, 0),
-                      child: const Icon(
-                        Icons.search,
-                        color: Colors.yellow, // Updated icon color to yellow
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: TextField(
-                      controller: searchController,
-                      style: const TextStyle(color: Colors.greenAccent),
-                      onSubmitted: (value) {},
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "BreakingNews",
-                        hintStyle: const TextStyle(
-                            color: Colors
-                                .yellow), // Updated hint text color to yellow
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            // Add your action here
-                          },
-                          icon: const Icon(
-                            Icons.favorite,
-                            color:
-                                Colors.yellow, // Updated icon color to yellow
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SafeArea(
-            child: Container(
-              height: 50,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  shrinkWrap: true,
-                  itemCount: navBarItem.length,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: () {
-                        print("$navBarItem");
-                      },
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SafeArea(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+                decoration: BoxDecoration(
+                  color: Colors.black, // Updated background color to black
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {},
                       child: Container(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                        margin: EdgeInsets.symmetric(horizontal: 5),
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(15),
+                        margin: const EdgeInsets.fromLTRB(3, 0, 7, 0),
+                        child: const Icon(
+                          Icons.search,
+                          color: Colors.yellow, // Updated icon color to yellow
                         ),
-                        child: Center(
-                          child: Text(
-                            navBarItem[index],
-                            style: const TextStyle(
-                              fontSize: 20,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Expanded(
+                      child: TextField(
+                        controller: searchController,
+                        style: const TextStyle(color: Colors.greenAccent),
+                        onSubmitted: (value) {},
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "BreakingNews",
+                          hintStyle: const TextStyle(
+                              color: Colors
+                                  .yellow), // Updated hint text color to yellow
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              // Add your action here
+                            },
+                            icon: const Icon(
+                              Icons.favorite,
+                              color:
+                                  Colors.yellow, // Updated icon color to yellow
                             ),
                           ),
                         ),
                       ),
-                    );
-                  }),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
-          CarouselSlider(
-            options: (CarouselOptions(
-              height: 200,
-              enableInfiniteScroll: false,
-              autoPlay: true,
-              enlargeCenterPage: true,
-            )),
-            items: itemList.map((itemList) {
-              return Builder(builder: (BuildContext context) {
-                return Container(
-                  margin: EdgeInsets.symmetric(horizontal: 5,vertical: 10),
-                  child: itemList,
-                );
-              });
-            }).toList(),
-          )
-        ],
+            SafeArea(
+              child: Container(
+                margin: EdgeInsets.all(20),
+                height: 50,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: navBarItem.length,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {
+                          print("$navBarItem");
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          margin: EdgeInsets.symmetric(horizontal: 5),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Center(
+                            child: Text(
+                              navBarItem[index],
+                              style: const TextStyle(
+                                fontSize: 18,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+            ),
+            Container(
+              child: CarouselSlider(
+                options: (CarouselOptions(
+                  height: 200,
+                  enableInfiniteScroll: false,
+                  autoPlay: true,
+                  enlargeCenterPage: true,
+                )),
+                items: itemList.map((itemList) {
+                  return Builder(builder: (BuildContext context) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                      child: itemList,
+                    );
+                  });
+                }).toList(),
+              ),
+            ),
+            Container(
+              child: ListView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: 3,
+                  itemBuilder: (context, index) {
+                    return Container(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                        child: Card(
+                          elevation: 5,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Stack(
+                            children: [
+                              ClipRRect(
+                                  borderRadius: BorderRadius.circular(15),
+                                  child:
+                                      Image.asset("images/breakingNews.jpeg")),
+                              Positioned(
+                                left: 0,
+                                right: 0,
+                                bottom: 0,
+                                child: Container(
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(15),
+                                        gradient: LinearGradient(
+                                          colors: [
+                                            Colors.black.withOpacity(0),
+                                            Colors.black,
+                                          ],
+                                          begin: Alignment.topCenter,
+                                          end: Alignment.bottomCenter,
+                                        )),
+                                    padding: EdgeInsets.fromLTRB(10, 15, 10, 8),
+                                    child: const Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          'NEWS HEADLINE',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18,
+                                              color: Colors.white),
+                                        ),
+                                        Text(
+                                          "News of the day",
+                                          style: TextStyle(
+                                              fontSize: 15, color: Colors.grey),
+                                        ),
+                                      ],
+                                    )),
+                              )
+                            ],
+                          ),
+                        ));
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
 
   final List itemList = [
-   Text("Who"),
+    Text("Who"),
     Text("are"),
     Text("you"),
     Text("Narendra Modi")
